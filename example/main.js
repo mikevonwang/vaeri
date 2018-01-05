eccffeeb050a2b92aab567336b752ec188250dc1
mikevonwang <mikevonwang@gmail.com>
2018-01-04 21:10:57 -0600

    basic class structures

--- example/main.js
+++ example/main.js
@@ -1,5 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
+class App extends Vaeri {
 
-  
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
 
-});
+  getDOM() {
+    return ({
+      main: 'main',
+      cards: ['div.card'],
+      list: 'ul.list',
+      list_items: ['ul.list > li'],
+    });
+  }
+
+  setControllers() {
+    return ({
+      data: this.ctlrData,
+    });
+  }
+
+  onMount() {
+    this.setState({
+      data: [
+        {
+          title: 'when the seas',
+        }, {
+          title: 'and mountains fall',
+        }
+      ],
+    });
+  }
+
+  ctlrData(prev_state, new_state) {
+    var new_list_items = '';
+    new_state.data.forEach((c) => {
+      new_list_items += '<li>' + c.title + '</li>'
+    });
+    this.populate(this.dom.list, new_list_items);
+  }
+
+}
+
+(new App()).start();
--- src/vaeri.js
+++ src/vaeri.js
@@ -1,3 +1,48 @@
-function v(x) {
-  return document.querySelectorAll(x);
+class Vaeri {
+  constructor() {
+    this.state = {};
+    this.controllers = {};
+    this.dom = {};
+  }
+
+  start() {
+    document.addEventListener('DOMContentLoaded', () => {
+
+      if (this.getDOM) {
+        const user_dom = this.getDOM();
+        const mapped_dom = {};
+        Object.keys(user_dom).forEach((c) => {
+          if (typeof user_dom[c] === 'string') {
+            mapped_dom[c] = document.querySelector(user_dom[c]);
+          }
+          else {
+            mapped_dom[c] = Array.prototype.slice.call(document.querySelectorAll(user_dom[c]));
+          }
+        });
+        this.dom = mapped_dom;
+      }
+
+      if (this.setControllers) {
+        this.controllers = Object.assign({}, this.setControllers());
+      }
+
+      if (this.onMount) {
+        this.onMount();
+      }
+
+    });
+  }
+
+  setState(new_state_items) {
+    const new_state = Object.assign({}, this.state, new_state_items);
+    Object.keys(new_state_items).forEach((c) => {
+      this.controllers[c].call(this, this.state, new_state);
+    });
+    this.state = new_state;
+  }
+
+  populate(parent, html) {
+    parent.insertAdjacentHTML('beforeEnd', html);
+  }
+
 }
